eb4cd5a0933e13977622ad60490f2bf25f39292a
KwebdesignAK <kristensen.anja78@gmail.com>
2025-11-13 19:31:10 +0100

    Update App.vue
    Opdateret template og scripts

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
-  <div id="root">
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-
-    <HelloWorld msg="Vite + Vue" />
-
-    <Messages />
+  <div>
+    <Header />
+    <Hero />
+    <InfobBoxes />
+    <router-view />
+    <Footer />
   </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Messages from './components/Messages.vue'
+import Header from './components/Header.vue'
+import Seachbar from ',components/Searchbar.vue'
+import Hero from './components/Hero.vue'
+import Infoboxes from ',/components/InfoBoxes.vue'
+import EventFooter from './components/Footer.vue'
 </script>
 
 <style scoped>
